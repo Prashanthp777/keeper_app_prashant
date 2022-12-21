63dd1001017e928cbbd16c7d1ea8931d1e07cda2
unknown <prashant.kumar.min19@itbhu.ac.in>
2022-12-21 23:10:20 +0530

    fixed react build error

---
+++ build/precache-manifest.27e7e0280ef900953fd8c083d00c9c30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e344172f0f51702b830685a2bd7145e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a8ce9d867e3d9aab4d",
+    "url": "/static/js/2.7358e7b1.chunk.js"
+  },
+  {
+    "revision": "075a3ee7d8511b680c67",
+    "url": "/static/js/main.d6644ca4.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
